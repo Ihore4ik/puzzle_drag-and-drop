463922f681c2d823108d5bafc41a3418a09a7207
Ihor Triapitsyn <tryapitsin1990@gmail.com>
2023-09-20 13:32:03 +0300

    Added styles-classes

--- script/script.js
+++ script/script.js
@@ -1,12 +1,16 @@
 const blocks = document.querySelectorAll(".block");
 const dragable_items = document.querySelectorAll("img");
-// const dragable_items = document.querySelectorAll(".item");
 
 const dragStartHandler = (e) => {
   e.dataTransfer.setData("id", e.target.id);
+  e.target.classList.add("dragging");
 };
 const dragOverHandler = (e) => {
   e.preventDefault();
+  e.currentTarget.classList.add("hovered");
+};
+const dragLeaveHandler = (e) => {
+  e.currentTarget.classList.remove("hovered");
 };
 const dropHandler = (e) => {
   e.preventDefault();
@@ -14,16 +18,19 @@ const dropHandler = (e) => {
   let dragableElement = document.getElementById(id);
   let parentDragableElement = dragableElement.parentNode;
   let replaceableElement = e.currentTarget.firstElementChild;
-
+  dragableElement.classList.remove("dragging");
+  e.currentTarget.classList.remove("hovered");
   if (replaceableElement.id !== id) {
     e.currentTarget.replaceChild(dragableElement, replaceableElement);
     parentDragableElement.appendChild(replaceableElement);
   }
 };
+
 dragable_items.forEach((item) => {
   item.addEventListener("dragstart", dragStartHandler);
 });
 blocks.forEach((block) => {
   block.addEventListener("dragover", dragOverHandler);
+  block.addEventListener("dragleave", dragLeaveHandler);
   block.addEventListener("drop", dropHandler);
 });
